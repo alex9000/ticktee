779861172ee86e7ed7f279d252a395ad98f2c0e7
Alex <alex9000@gmail.com>
2012-07-07 17:43:45 -0700

    Added admin namespaced users controller

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,13 +1,9 @@
 require 'spec_helper'
 
 describe ProjectsController do
-  let(:user) do
-    user = Factory(:user)
-    user.confirm!
-    user
-  end
+  let(:user) { create_user! }
   
-  let(:project) {Factory(:project)}
+  let(:project) { Factory(:project) }
   
   context "standard users" do
     { "new" => "get",
