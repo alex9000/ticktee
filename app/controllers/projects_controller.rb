42c94b2539da90e1e714b59876075afcaf38ae1a
Alex <alex9000@gmail.com>
2012-08-05 17:48:49 -0700

    Made projects only visible to users with permissions to see them

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
-  before_filter :authorize_admin!, :except => [:index, :show]
+  before_filter :authorize_admin!, :except => [:index, :show]   
+  before_filter :authenticate_user!, :only => [:show]
   before_filter :find_project, :only => [:show,
                                         :edit,
                                         :update,
@@ -49,8 +50,12 @@ class ProjectsController < ApplicationController
   end
   
   private
-    def find_project
-      @project = Project.find(params[:id])
+    def find_project  
+      @project = if current_user.admin?
+        Project.find(params[:id])
+      else
+        @project = Project.readable_by(current_user).find(params[:id])  
+      end
       rescue ActiveRecord::RecordNotFound
       flash[:alert] = "The project you were looking" +
         " for could not be found."
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -17,13 +17,22 @@ describe ProjectsController do
           response.should redirect_to(root_path)
           flash[:alert].should eql ("You must be an admin to do that.")
         end
+      end       
+      
+      it "cannot acess the show action" do
+          sign_in(:user, user) 
+          get :show, :id => project.id
+          response.should redirect_to(projects_path)
+          flash[:alert].should eql ("The project you were looking for could not be found.")  
       end
   end
   
-  it "displays an error for a missing project" do
+  it "displays an error for a missing project" do   
+     sign_in(:user, user)  
     get :show, :id => "not-here"
     response.should redirect_to(projects_path)
     message = "The project you were looking for could not be found."
     flash[:alert].should eql(message)
-  end
+  end  
+  
 end
