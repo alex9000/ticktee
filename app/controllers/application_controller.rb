3aa082d288f6c48ffd7223bdf7f4b6af3266ab12
Alex <alex9000@gmail.com>
2012-11-04 17:59:24 -0800

    Added Admin::StatesController for managing states

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :find_states
   
   private
   
@@ -10,4 +11,8 @@ class ApplicationController < ActionController::Base
      redirect_to root_path
     end
   end
+  
+  def find_states
+     @states = State.all
+   end
 end
