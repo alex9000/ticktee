c456881f3a1ba6668d8bd286d7a07b153d4a99b8
Alex <alex9000@gmail.com>
2012-11-18 17:08:48 -0800

    Users can add tags when adding a comment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,14 +7,15 @@ class CommentsController < ApplicationController
      params[:comment].delete(:state_id)
     end
     @comment = @ticket.comments.build(params[:comment].merge(:user => current_user))
-      if @comment.save
-        flash[:notice] = "Comment has been created."
-        redirect_to [@ticket.project, @ticket]
-      else
-        @states = State.all
-        flash[:alert] = "Comment has not been created."
-        render :template => "tickets/show"
-      end
+    if @comment.save
+      @ticket.tag!(params[:tags])
+      flash[:notice] = "Comment has been created."
+      redirect_to [@ticket.project, @ticket]
+    else
+      @states = State.all
+      flash[:alert] = "Comment has not been created."
+      render :template => "tickets/show"
+    end
   end
   
   private
