d12c1a2d2d7d53fbfffa9522fc0910d2f6ab148a
Alex <alex9000@gmail.com>
2012-12-08 15:27:27 -0800

    Users are now automatically subscribed to a ticket when they comment on it

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   before_create :set_previous_state
   after_create :set_ticket_state
+  after_create :creator_watches_ticket
   
   belongs_to :ticket
   belongs_to :user
@@ -20,6 +21,10 @@ class Comment < ActiveRecord::Base
     def set_ticket_state
       self.ticket.state = self.state
       self.ticket.save!
+    end
+    
+    def creator_watches_ticket
+      ticket.watchers << user
     end
 
 end
